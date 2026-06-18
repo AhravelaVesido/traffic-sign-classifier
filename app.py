c1f1fd072832f282fce12c356d8b03c72912beb0
AhravelaVesido <ahravelavesido@gmail.com>
2026-06-18 21:04:24 +0800

    Fis the port binding for Render deployment

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def latest():
 
 if __name__ == "__main__":                                    # Run only if executed directly
     port = int(os.environ.get("PORT", 5000))                  # Render's assigned port for deployment
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)  # Run Flask with SocketIO, debug=True shows errors
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)  # Run Flask with SocketIO, debug=True shows errors
